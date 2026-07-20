5ae30d42e0c39c0e9d7be70cd7b99dc3f2155878
Ezee Kits <ezeekits@gmail.com>
2026-07-20 16:56:25 +0100

    Delete main_task directory

--- main_task/tasks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Task(models.Model):
-    content = models.TextField()
-    complete = models.BooleanField(default=False)
-    date_added = models.DateTimeField(default=timezone.now)
-    def __str__(self):
-        return self.content
--- main_task/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('',views.home,name = 'home_page'),
-    path('update/<str:pk>/',views.update,name = 'update_page'),
-    path('delete/<str:pk>/',views.delete,name = 'delete_page'),
-]
