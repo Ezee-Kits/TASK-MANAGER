7b05d106b869202a69457ed0f211981305cc7dda
Ezee Kits <ezeekits@gmail.com>
2026-07-20 16:53:33 +0100

    Update settings.py

--- main_task/main_task/settings.py
+++ main_task/main_task/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+tbjx^gv*c=iv=rj_)gx8llim$gtohj*g$4ur(+gh02oigf4%('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
