01a0fa4b1d7b6f640eb95296a6296a548f68b029
Ezee Kits <ezeekits@gmail.com>
2026-07-20 16:58:37 +0100

    Add files via upload

---
+++ tasks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+
+class Task(models.Model):
+    content = models.TextField()
+    complete = models.BooleanField(default=False)
+    date_added = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.content
